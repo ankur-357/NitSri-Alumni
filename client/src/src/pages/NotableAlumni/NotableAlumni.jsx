ec8e70c9faf607a297d9a475a8fed494ee8a5183
ANKUR KUMAR <96306426+ankur-357@users.noreply.github.com>
2025-04-20 11:33:52 +0530

    Update NotableAlumni.jsx

--- client/src/src/pages/NotableAlumni/NotableAlumni.jsx
+++ client/src/src/pages/NotableAlumni/NotableAlumni.jsx
@@ -147,31 +147,51 @@ export default NotableAlumni;
 
 
 const AlumniCard = ({ alum, refetch }) => {
-
   const handleDelete = async () => {
     try {
-      await Promise.all([deleteFile(alum.image), deleteDocument("notable-alumni", alum.$id)]);
+      await Promise.all([
+        deleteFile(alum.image),
+        deleteDocument("notable-alumni", alum.$id)
+      ]);
       refetch();
       toast.success("Alumni deleted successfully!");
     } catch (error) {
       toast.error(error.message);
     }
-  }
-
+  };
 
   return (
-    <div data-aos="fade-in" className='rounded-3xl shadow-lg bg-[#000000] hover:bg-gray-950 border border-gray-900'>
-      <div className='relative flex hover:scale-[98%] transition lg:flex-row md:flex-col flex-col lg:items-start items-center justify-start  lg:py-6 py-10 lg:gap-3 md:gap-3 gap-6 px-5 lg:max-w-full max-w-[24rem] m-auto'>
-        <div className='relative lg:h-24 md:h-32 h-36 lg:w-24 md:w-32 w-36 rounded-full flex items-center justify-center lg:min-w-[6rem]'>
-          <img loading='lazy' className=' lg:h-24 md:h-32 h-36 lg:w-24 md:w-32 w-36 rounded-full' src={getImageURL(alum.image)} alt="project" />
-          <FaStar className='absolute bottom-0 lg:text-3xl text-5xl right-0 text-[#ffc547]' />
+    <div
+      data-aos="fade-in"
+      className="relative rounded-3xl shadow-lg bg-[#000000] hover:bg-gray-950 border border-gray-900"
+    >
+      <button
+        onClick={handleDelete}
+        className="absolute top-4 right-4 text-red-500 hover:text-red-700 cursor-pointer z-10"
+        title="Delete Alumni"
+      >
+        <FaTrash size={20} />
+      </button>
+
+
+      <div className="relative flex hover:scale-[98%] transition lg:flex-row md:flex-col flex-col lg:items-start items-center justify-start lg:py-6 py-10 lg:gap-3 md:gap-3 gap-6 px-5 lg:max-w-full max-w-[24rem] m-auto">
+        <div className="relative lg:h-24 md:h-32 h-36 lg:w-24 md:w-32 w-36 rounded-full flex items-center justify-center lg:min-w-[6rem]">
+          <img
+            loading="lazy"
+            className="lg:h-24 md:h-32 h-36 lg:w-24 md:w-32 w-36 rounded-full"
+            src={getImageURL(alum.image)}
+            alt="alumni-profile"
+          />
+          <FaStar className="absolute bottom-0 lg:text-3xl text-5xl right-0 text-[#ffc547]" />
         </div>
-        <div className='w-full flex flex-col lg:text-left text-center'>
-          <h3 className='text-xl font-semibold text-sky-500'>{alum.name}</h3>
-          <h3 className='mb-2'>({alum.designation})</h3>
-          <p className='lg:text-base text-sm text-justify text-gray-400 md:block'>{alum.about}</p>
+        <div className="w-full flex flex-col lg:text-left text-center">
+          <h3 className="text-xl font-semibold text-sky-500">{alum.name}</h3>
+          <h3 className="mb-2">({alum.designation})</h3>
+          <p className="lg:text-base text-sm text-justify text-gray-400 md:block">
+            {alum.about}
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
