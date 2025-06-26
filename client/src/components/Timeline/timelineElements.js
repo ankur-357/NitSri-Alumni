54d7ca262f818ffce8216532b50ac2b91367d804
Ankur Kumar <ankurtiwari502@gmail.com>
2025-06-26 07:27:00 +0530

    new change done

--- client/src/components/Timeline/timelineElements.js
+++ client/src/components/Timeline/timelineElements.js
@@ -1,41 +1,25 @@
 const timelineElements = [
   {
-    id: 1,
-    title: "1876-1882",
-    description:
-      " Establishment of survey schools in Dacca, Hoogly, Cuttack, and Srinagar. Schools at Hoogly and Cuttack failed, while those at Dacca and Srinagar continued to prosper. In 1882, J&K Industrial School was opened in the premises of the survey school in Srinagar college campus.",
-    date: "",
-    icon: "work",
-  },
-  {
-    id: 2,
-    title: "1886",
-    description:
-      "In 1924, the Pleaders Survey Training School evolved into the J&K College of Engineering Srinagar. This transformation positioned the institute as the 6th oldest engineering institute in India.",
-    date: "",
-    icon: "school",
-  },
-  {
-    id: 3,
-    title: "1892-1902",
-    description:
-      "Construction of a functional building, which is now the main administrative building of NIT Srinagar, with a donation of Rs. 100,000 from Nawab Lulf Ali Khan of Srinagar City. An additional Rs. 38,000 came from the Prince of Wales reception fund. Prof. C. R. Walford served as the Principal of the college during this period.",
-    date: "",
-    icon: "work",
-  },
-  {
     id: 4,
-    title: "1924",
+    title: "1960",
     description:
-      "J&K School of Engineering upgraded to J&K College of Engineering under Srinagar University.",
+      "NIT Srinagar was established in 1960 as the Regional Engineering College, Srinagar.  It was one of the first eight Regional Engineering Colleges established by the Government of India during the first Five-Year Plan.",
     date: "",
     icon: "school",
   },
   {
     id: 5,
+    title: "2003",
+    description:
+      " The Regional Engineering College, Srinagar, was upgraded to become the National Institute of Technology, Srinagar, in July 2003",
+    date: "",
+    icon: "work",
+  },
+  {
+    id: 6,
     title: "2004",
     description:
-      "Ministry of Human Resource Development (MHRD), Government of India upgraded J&K College of Engineering to National Institute of Technology Srinagar.",
+      " In this year, the institution was granted Deemed University status with the approval of the University Grants Commission, All India Council of Technical Education , and the Government of India.",
     date: "",
     icon: "work",
   },
@@ -43,7 +27,7 @@ const timelineElements = [
     id: 6,
     title: "2007",
     description:
-      "NIT Srinagar was brought under the act 'NIT Act 2007' and declared an Institute of National Importance. ",
+      "On 15 August 2007, it became an Institute of National Importance[6] under the NIT Bill passed by the parliament of India. ",
     date: "",
     icon: "school",
   },
@@ -51,7 +35,7 @@ const timelineElements = [
     id: 7,
     title: "Present Day",
     description:
-      " NIT Srinagar offers undergraduate programs (B.Tech & B.Arch), postgraduate program (M.Tech & MRUP), and Ph.D. programs with a total student population of nearly 3000. The institute has state-of-the-art laboratories in each department.Notable alumni include Mr. M. B. Verma, Dr. Ratan K. Sinha, and Mr. C. Prasad. - The institute maintains active industry linkages and collaborations, with plans to sign MoUs with organizations like FICCI and CII. Graduates of NIT Srinagar are serving the Indian Industry and Public Sector Undertakings (PSUs) with distinction. ",
+      " NIT Srinagar offers undergraduate programs (B.Tech & M.tech), postgraduate program (M.Tech & MBA), and Ph.D. programs with a total student population of nearly 3000. The institute has state-of-the-art laboratories in each department.Notable alumni include Mr. Sonam Wangchuk, Dr. S.S. Bajaj, and Mr. Daud Danesh Jafar - The institute maintains active industry linkages and collaborations, with plans to sign MoUs with organizations like FICCI and CII. Graduates of NIT Srinagar are serving the Indian Industry and Public Sector Undertakings (PSUs) with distinction. ",
     date: "",
     icon: "school",
   },
@@ -59,7 +43,7 @@ const timelineElements = [
     id: 8,
     title: "Future (2024-25)",
     description:
-      "The future of NIT Srinagar includes the inauguration of a state-of-the-art campus in Sikandarpur, near Srinagar, covering an expansive area of 125 acres. This ambitious project is poised to significantly enhance the institute's infrastructure and facilities. The commencement of the shifting process is anticipated to coincide with the academic session of 2024-25",
+      "The future of NIT Srinagar includes the inauguration of a state-of-the-art campus in Newa Pulwama, near Srinagar, covering an expansive area of 607 acres. This ambitious project is poised to significantly enhance the institute's infrastructure and facilities. ",
     date: "",
     icon: "school",
   },
